e9721d2f05193598858b196702e157e92ed2f1f3
mirajHossen99 <mirajhossen211013@gmail.com>
2026-07-03 00:01:13 +0600

    module added

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './module/user/user.module';
+import { AuthModule } from './module/auth/auth.module';
+import { ProductModule } from './module/product/product.module';
+import { OrderModule } from './module/order/order.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, AuthModule, ProductModule, OrderModule],
   controllers: [AppController],
   providers: [AppService],
 })
