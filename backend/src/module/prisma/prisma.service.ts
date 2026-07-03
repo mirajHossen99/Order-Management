f1d9ccf4a72ce676354d5b92130153a473b082c4
mirajHossen99 <mirajhossen211013@gmail.com>
2026-07-03 13:12:44 +0600

    package install

--- backend/src/module/prisma/prisma.service.ts
+++ backend/src/module/prisma/prisma.service.ts
@@ -5,23 +5,23 @@ import {
   OnModuleInit,
 } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-// import { PrismaClient } from '@prisma/client';
 import { PrismaClient } from '../../../prisma/generated/prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
-export class PrismaService implements OnModuleInit, OnModuleDestroy {
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
   private readonly logger = new Logger(PrismaService.name);
-  private readonly prisma: PrismaClient;
-  private readonly connectionString: string;
 
   constructor(private readonly configService: ConfigService) {
-    this.connectionString =
-      this.configService.getOrThrow<string>('DATABASE_URL');
+    const connectionString =
+      configService.getOrThrow<string>('DATABASE_URL');
 
-    const adapter = new PrismaPg({ connectionString: this.connectionString });
+    const adapter = new PrismaPg({ connectionString });
 
-    this.prisma = new PrismaClient({
+    super({
       adapter,
       log: [{ emit: 'event', level: 'error' }],
     });
@@ -29,17 +29,13 @@ export class PrismaService implements OnModuleInit, OnModuleDestroy {
 
   async onModuleInit() {
     this.logger.log('[INIT] Prisma connecting...');
-    await this.prisma.$connect();
+    await this.$connect();
     this.logger.log('[INIT] Prisma connected');
   }
 
   async onModuleDestroy() {
     this.logger.log('[DESTROY] Prisma disconnecting...');
-    await this.prisma.$disconnect();
+    await this.$disconnect();
     this.logger.log('[DESTROY] Prisma disconnected');
-  }
-
-  get client() {
-    return this.prisma;
   }
 }
